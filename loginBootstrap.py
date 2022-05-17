804d2c2680ce2cbf4cb9e46d10e705eca6afb575
arielAayala <ariel170802@gmail.com>
2022-05-17 16:34:02 -0300

    Update loginBootstrap.py

--- loginBootstrap.py
+++ loginBootstrap.py
@@ -1,8 +1,8 @@
 import ttkbootstrap as ttk
 
 pantalla = ttk.Window(themename="darkly")
+pantalla.geometry("400x200")
+ttk.Label(pantalla,text="Ingrese su datos").place(x=20,y=20)
 
-print("hola")
-print("q")
-print("chau4")
+
 pantalla.mainloop()
