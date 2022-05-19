49318233b49b840c764d5c045a55f830224c13dc
arielAayala <ariel170802@gmail.com>
2022-05-18 21:26:09 -0300

    probando xd

--- probandoContra.py
+++ probandoContra.py
@@ -1,9 +1,6 @@
+from encodings import utf_8
 from lib2to3.pgen2 import token
 from msilib.schema import FeatureComponents
 from cryptography.fernet import Fernet
 key = Fernet.generate_key()
 f= Fernet(key)
-token = f.encrypt(b"hola")
-print(token)
-token = f.decrypt(token)
-print(token)
