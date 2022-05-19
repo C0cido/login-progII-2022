92a20f9a1a71bbd88c96144945c124853d94ebc1
arielAayala <ariel170802@gmail.com>
2022-05-18 22:47:46 -0300

    hay error en el login

--- contra.py
+++ contra.py
@@ -1,6 +1,3 @@
-from encodings import utf_8
-from lib2to3.pgen2 import token
-from msilib.schema import FeatureComponents
 from cryptography.fernet import Fernet
 key = Fernet.generate_key()
 f = Fernet(key)
---
+++ login.py
@@ -0,0 +1,46 @@
+import ttkbootstrap as ttk
+import json
+import contra as encriptado
+from tkinter import messagebox as ms
+
+pantalla = ttk.Window(themename="darkly")
+pantalla.title("Login de usuarios")
+pantalla.geometry("280x180")
+
+#variables
+varNombre = ttk.StringVar(pantalla,"")
+varContra = ttk.StringVar(pantalla,"") 
+
+#funciones
+def login():
+    lstUsuario = []
+    try:
+        with open("contra.json") as lst:
+            lstUsuario=json.load(lst)
+    except:
+        lstUsuario = []
+    if len(varContra.get()) > 0 and len(varNombre.get()) > 0: 
+        for i in lstUsuario:
+            print(encriptado.f.decrypt(bytes(i["Contra"],"utf_8")))
+            if (i["Usuario"] == varNombre.get()) and ((encriptado.f.decrypt(bytes(i["Contra"]))).decode("utf_8") == varContra.get()):
+                ms.showinfo("Login Existoso","Se ha logueado existosamente")
+    else:
+        ms.showerror("Error","Ingrese la informacion necesaria")
+
+#Nombre
+ttk.Label(pantalla,text="Nombre").place(x=20,y=20)
+entNombre = ttk.Entry(pantalla,textvariable=varNombre)
+entNombre.place(x=120,y=20)
+entNombre.focus()
+
+#contra
+ttk.Label(pantalla,text="Contra").place(x=20,y=60)
+entContra = ttk.Entry(pantalla,textvariable=varContra)
+entContra.place(x=120,y=60)
+
+#buton cargar datos
+btnCargar = ttk.Button(pantalla,command=login,text="Loguear")
+btnCargar.place(x=70,y=100)
+
+#Inciar
+pantalla.mainloop()
