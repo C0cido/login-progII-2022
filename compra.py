d3a05e8b852c3d297a4919c4c53d7c960cf33b89
arielAayala <ariel170802@gmail.com>
2022-05-24 03:33:16 -0300

    crearCompras

--- compra.py
+++ compra.py
@@ -1,22 +1,24 @@
 import json
 import ttkbootstrap as ttk
+import funciones as fn
+
+lstInventario = fn.abrirArchivo("archivosJSON/inventario.json")
 
 #estructura del menu de compras
-def abrirCompra():
+def menuCompra():
     menuCompra = ttk.Window(themename="darkly")
-    menuCompra.geometry("400x400")
-    menuCompra.title("Sector compras")
-    tabControl=ttk.Notebook(menuCompra)
-    tab1 = ttk.Frame(tabControl)
-    tab2 = ttk.Frame(tabControl)
-    tabControl.add(tab1,text="tab1")
-    tabControl.add(tab2,text="tab2")
-    tabControl.pack(expand = 1, fill ="both")
-    ttk.Label(tab1,text="Bienvenido").place(x=20,y=20)
+    menuCompra.geometry("800x400")
+    menuCompra.title("Sector deposito")
+    ttk.Label(text="Bienvenido").place(x=20,y=20)
+    tblInventario = ttk.Treeview(columns=("NombreProducto","StockProducto"))
+    tblInventario.heading("#0", text="NombreProducto")
+    tblInventario.heading("NombreProducto", text="NombreProducto")
+    tblInventario.heading("StockProducto", text="StockProducto")
+    tblInventario.place(x=20,y=60)
 
 
 
 
 
     menuCompra.mainloop()
-abrirCompra()
+menuCompra()
