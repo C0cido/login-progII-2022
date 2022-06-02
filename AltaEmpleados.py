8dd7d7df53a166373f6ae7b70679d6e1952b0e28
JuanAngelBi <105744962+JuanAngelBi@users.noreply.github.com>
2022-06-02 16:15:54 -0300

    Alta de Empleados
    :)

--- AltaEmpleados.py
+++ AltaEmpleados.py
@@ -3,6 +3,7 @@ import json
 import tkinter.messagebox as msg
 
 lstEmpleados = []
+lstUsuarios = []
 try:
     with open ("empleados.json") as empleados:
         lstEmpleados = json.load(empleados)
@@ -43,30 +44,39 @@ def fDNIenLista(DNI):
     
 
 def fRegistrarEmpleado():
-    if fDNIenLista(varDNI.get()) and varNombre.get().isalpha and varApellido.get().isalpha and (str(varDNI.get()).isdigit and len(varDNI.get() == 8)) and ((str(varCUIL.get()).isdigit) and len(varCUIL.get()) == 11):
+    if fDNIenLista(varDNI.get()) and varNombre.get().isalpha and varApellido.get().isalpha and varDNI.get().isdigit and (len(varDNI.get())== 8) and varCUIL.get().isdigit and (len(varCUIL.get()) == 11):
         nEmpleado = {}
         try:
             with open ("empleados.json") as archivo:
                 lstEmpleados = json.load(archivo)
         except:
             lstEmpleados = []
-        nEmpleado["IDEmpleado"] = fContID()
+        idVinculada = fContID()
+        nEmpleado["IDEmpleado"] = idVinculada
         nEmpleado["Nombre"] = varNombre.get() + " " + varApellido.get()
-        nEmpleado["Contra"] = varContra.get()
         nEmpleado["DNI"] = varDNI.get()
         nEmpleado["CUIL"] = varCUIL.get()
         nEmpleado["Sector"] = cmbSector.get()
         lstEmpleados.append(nEmpleado)
         with open("empleados.json", "w") as empleados:
             json.dump(lstEmpleados, empleados)
+        with open("usuario.json") as usuarios:
+            lstUsuario
+        msg.showinfo("Registro", "Registro exitoso.")
+        nUsuario = {}
+        nUsuario["IDUsuario"] = idVinculada
+        nUsuario["Nombre"] = varNombre.get() + " " + varApellido.get()
+        nUsuario["Contra"] = varDNI.get()
+
     else:
+        print(len(varDNI.get()))
         if (varNombre.get().isalpha == False) and (varApellido.get() == False):
             msg.showerror("Registro", "El nombre solo puede contener letras.")
-        elif str(varDNI.get()).isdigit == False:
+        elif varDNI.get().isdigit == False:
             msg.showerror("Registro", "El DNI solo puede contener números.")
         elif len(varDNI.get()) != 8:
             msg.showerror("Registro", "El DNI solo puede tener 8 números.")
-        elif str(varCUIL.get()).isdigit == False:
+        elif varCUIL.get().isdigit == False:
             msg.showerror("Registro", "El CUIL solo puede contener números.")
         elif len(varCUIL.get()) != 11:
             msg.showerror("Registro", "El CUIL solo puede tener 11 números.")
@@ -76,19 +86,19 @@ varNombre = ttk.StringVar()
 ttk.Entry(altaEmpleados, textvariable=varNombre).place(x=150, y=140)
 ttk.Label(altaEmpleados, text="Apellido:").place(x=150, y=180)
 varApellido = ttk.StringVar()
-ttk.Entry(altaEmpleados, textvariable=varNombre).place(x=150, y=220)
+ttk.Entry(altaEmpleados, textvariable=varApellido).place(x=150, y=220)
 ttk.Label(altaEmpleados, text="Contraseña:").place(x=150, y=260)
-varContra = ttk.StringVar()
-ttk.Entry(altaEmpleados).place(x=150, y=300)
+
 ttk.Label(altaEmpleados, text="DNI:").place(x=150, y=340)
-varDNI = ttk.IntVar()
-ttk.Entry(altaEmpleados).place(x=150, y=380)
+varDNI = ttk.StringVar()
+ttk.Entry(altaEmpleados, textvariable=varDNI).place(x=150, y=380)
 ttk.Label(altaEmpleados, text="CUIL:").place(x=150, y=420)
-varCUIL = ttk.IntVar()
-ttk.Entry(altaEmpleados).place(x=150, y=460)
+varCUIL = ttk.StringVar()
+ttk.Entry(altaEmpleados, textvariable=varCUIL).place(x=150, y=460)
 ttk.Label(altaEmpleados, text="Sector:").place(x=150, y=500)
-lstSectores = []
+lstSectores = ["Ventas", "Stock"]
 cmbSector = ttk.Combobox(altaEmpleados, values=lstSectores, state="readonly")
 cmbSector.place(x=150, y=540)
+ttk.Button(altaEmpleados, text="Registrar", command=fRegistrarEmpleado).place(x=200, y=600)
 
 altaEmpleados.mainloop()
